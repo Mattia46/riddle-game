979086294819e3e0fe344a9f66f99b507f96c86f
Mattia46 <mattia.assogna@gmail.com>
2020-04-03 08:46:39 +0100

    added the riddle input

--- components/InputRiddle.js
+++ components/InputRiddle.js
@@ -5,19 +5,69 @@ import { Connect } from "aws-amplify-react-native";
 import { ScrollView } from 'react-native-gesture-handler';
 import { API, graphqlOperation } from 'aws-amplify';
 import { Input } from 'react-native-elements';
+import { Button } from 'react-native-elements';
+import CircleTimer from 'react-native-circle-timer';
+
+function UserSolution({solution, setSolution, shouldRender}) {
+  if(!shouldRender) return (
+    <Input
+      placeholder="Inseristi la tua risposta"
+      containerStyle={styles.input}
+      multiline={true}
+      underlineColorAndroid='transparent'
+      blurOnSubmit={true}
+      value={solution}
+      onChangeText={e => setSolution(e)}
+    />
+  );
+
+  return (
+    <View style={styles.solution}>
+      <Text> {solution} </Text>
+    </View>
+  )
+};
 
 function InputRiddle({riddle}) {
+  const [solution, setSolution] = useState('');
+  const [answered, setAnswered] = useState(false);
+  const [buttonValue, setButtonValue] = useState('Conferma');
+
+  const handler = () => {
+    setAnswered(!answered);
+    const value = buttonValue === 'Conferma'
+      ? 'Modifica'
+      : 'Conferma';
+    setButtonValue(value);
+  };
+
   return (
     <View>
       <View style={styles.solution}>
         <Text> {riddle.riddle} </Text>
       </View>
-      <Input
-        placeholder="Inseristi la tua risposta"
-        containerStyle={styles.input}
-        multiline={true}
-        underlineColorAndroid='transparent'
-        blurOnSubmit={true}
+      <CircleTimer
+        radius={35}
+        borderWidth={5}
+        seconds={180}
+        borderColor={'#F5F5F5'}
+        borderBackgroundColor={"#FF0000"}
+        onTimeElapsed={() => {
+          setAnswered(true);
+          alert('Time\'s up');
+        }}
+        showSecond={true}
+      />
+      <UserSolution
+        solution={solution}
+        shouldRender={answered}
+        setSolution={setSolution}
+      />
+      <Button
+        title={buttonValue}
+        type="outline"
+        onPress={handler}
+        containerStyle={styles.button}
       />
     </View>
   );
@@ -29,6 +79,11 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     //padding: 5,
   },
+  button: {
+    width: 200,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
   input: {
     //margin: 8,
     borderRadius: 24,
@@ -36,8 +91,8 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   solution: {
-    margin: 8,
-    borderRadius: 24,
+    margin: 12,
+    borderRadius: 14,
     backgroundColor: 'rgba(52, 52, 52, 0.4)',
     justifyContent: 'space-around',
     padding: 20,
