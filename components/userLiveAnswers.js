f192bf199a96daf66ee70b9c97304f5580986328
Mattia46 <mattia.assogna@gmail.com>
2020-04-04 14:23:50 +0100

    liveAvatar logic working fine

--- components/userLiveAnswers.js
+++ components/userLiveAnswers.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, Text, View } from 'react-native';
 import { API, graphqlOperation } from 'aws-amplify';
-import { Avatar } from "react-native-elements";
+import { Avatar, Badge } from "react-native-elements";
+import { ScrollView } from 'react-native-gesture-handler';
 import { getUserAnswer, onCreateAnswer } from './shared';
+
+function ShowBadge({user}) {
+  if(!user.hasAnswered) return null;
+
+  return <Badge status="success" />
+}
 
 function UserListAnwsers() {
   const [listUsers, setListUsers] = useState([]);
@@ -24,13 +31,13 @@ function UserListAnwsers() {
     API.graphql(graphqlOperation(getUserAnswer, {filter: { date: { eq:  today }}}));
 
   const onCreate = API.graphql(graphqlOperation(onCreateAnswer)).subscribe(data =>
-      updateUser(data.value.data.onCreateAnswer.user.id));
+    updateUser(data.value.data.onCreateAnswer.user.id));
 
 
   useEffect(() => {
     getListUsers().then(({data}) => {
       const normaliseList = data.listUsers?.items.map(user => ({
-        hasAnswered: user.answers?.items[0]?.result ? true : false,
+        hasAnswered: user.answers?.items[0]?.userSolution ? true : false,
         name: user.name,
         avatar: user.avatar,
         id: user.id
@@ -45,28 +52,31 @@ function UserListAnwsers() {
   return (
     <View>
       <Text>LIVE</Text>
-      <View style={styles.winner}>
+      <View style={styles.container}>
         { listUsers.map((user, index) => (
-          <Avatar key={index}
-            containerStyle={{padding: 3, backgroundColor: "#7CFC00", marginRight: 15,}}
-            rounded size={40}
-            source={{
-              uri:"https://img1.looper.com/img/gallery/the-5-best-and-5-worst-things-about-the-hulk-of-the-mcu/intro-1557524944.jpg"
-            }}
-          />
+          <View key={index} style={styles.avatar}>
+            <Avatar key={index}
+              rounded size={40}
+              source={{
+                uri:"https://img1.looper.com/img/gallery/the-5-best-and-5-worst-things-about-the-hulk-of-the-mcu/intro-1557524944.jpg"
+              }}
+            />
+            <ShowBadge user={user}/>
+          </View>
         )) }
       </View>
-
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  winner: {
+  container: {
     display: 'flex',
     justifyContent: 'flex-start',
-    marginLeft: 15,
-    padding: 10,
+    flexDirection: 'row',
+  },
+  avatar: {
+    display: 'flex',
     flexDirection: 'row',
   },
 });
