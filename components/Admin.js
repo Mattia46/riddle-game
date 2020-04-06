56f368fd75003056ef0570959012af95b7c8873a
Mattia46 <mattia.assogna@gmail.com>
2020-04-06 17:07:23 +0100

    added logic to add/create a new riddle

--- components/Admin.js
+++ components/Admin.js
@@ -2,19 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Input, Button, CheckBox } from 'react-native-elements';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { listUsers } from '../src/graphql/queries';
-import { onCreateUser } from '../src/graphql/subscriptions';
+import { API, graphqlOperation } from 'aws-amplify';
+import { riddleByDate } from '../src/graphql/queries';
+import { createRiddle, updateRiddle } from '../src/graphql/mutations';
 
 function Admin({user}) {
   if(!user || user.name !== 'mattia') return null;
-  const today = new Date().toISOString().split('T')[0]
 
+  const today = new Date().toISOString().split('T')[0]
+  const init = { expired: false, date: today};
   const [riddle, setRiddle] = useState({
     expired: false,
     date: today,
   });
 
-  const submit = () => console.log('here', riddle);
+  useEffect(() => {
+    API.graphql(graphqlOperation(riddleByDate, { date: today }))
+      .then(({data: { riddleByDate: { items }}}) => {
+        if(items.length > 0) {
+          return setRiddle(items[0]);
+        }
+        return setRiddle(init);
+      });
+  }, []);
+
+  const submit = () => {
+    if(riddle.id) {
+      return API.graphql(graphqlOperation(updateRiddle, { input: riddle }))
+        .then(({data: { updateRiddle }}) => alert(JSON.stringify(updateRiddle)));
+    }
+    return API.graphql(graphqlOperation(createRiddle, { input: riddle }))
+      .then(({data: { createRiddle }}) => alert(JSON.stringify(createRiddle)));
+  };
 
   return (
     <React.Fragment>
