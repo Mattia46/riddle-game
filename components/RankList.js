7f40caa6ce2a28bd3a865aa0fb8cd0844a8e91d7
Mattia46 <mattia.assogna@gmail.com>
2020-04-07 19:26:20 +0100

    fixed RankList avatars

--- components/RankList.js
+++ components/RankList.js
@@ -1,52 +1,71 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Avatar } from "react-native-elements";
-import { ListItem } from 'react-native-elements'
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Avatar, Badge, Icon } from "react-native-elements";
+
+function ShowBadge({user}) {
+  const status = user.answer ? 'success' : 'error';
+  console.log('s\ta\ ', status);
+
+  return <Badge
+    containerStyle={{ top: -2, right: 15 }}
+    status={status} value=""
+  />
+}
 
 function Rank ({usersAnswers}) {
   const usersCorrect = usersAnswers.filter(item => item.answer === true);
   const usersWrong = usersAnswers.filter(item => item.answer !== true);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.winner}>
-        { usersCorrect.map((user, index) => (
-          <Avatar key={index}
-            containerStyle={{padding: 3, backgroundColor: "#7CFC00", marginRight: 15,}}
-            rounded size={60}
+    <ScrollView>
+      { usersAnswers.map((user, index) => (
+        <View style={styles.container} key={index}>
+          <Avatar
+            rounded size={50}
             source={{
               uri:"https://img1.looper.com/img/gallery/the-5-best-and-5-worst-things-about-the-hulk-of-the-mcu/intro-1557524944.jpg"
             }}
           />
-        )) }
-      </View>
-      { usersWrong.map((user, index) => (
-        <ListItem
-          key={index}
-          leftAvatar={{
-            containerStyle: {marginLeft: 10, padding: 3, backgroundColor: "#FF0000"},
-            size: 50,
-            source: { uri: "https://img1.looper.com/img/gallery/the-5-best-and-5-worst-things-about-the-hulk-of-the-mcu/intro-1557524944.jpg" }
-          }}
-          title={user.solution}
-          bottomDivider
-        />
-      ))}
-    </View>
+          <Icon
+            name={user.answer ? 'check' : 'clear'}
+            color="white"
+            size={20}
+            containerStyle={[styles.icon, user.answer ? {backgroundColor: "#50F403"} : {backgroundColor: "#F42E03"}]}
+          />
+          <Text>{user.solution}</Text>
+        </View>
+      )) }
+    </ScrollView>
   )
 };
 
 const styles = StyleSheet.create({
+  icon: {
+    top: -6,
+    right: 20,
+    borderRadius: 25,
+    height: 20,
+    width: 20,
+  },
+  correct: {
+    backgroundColor: "#7CFC00",
+    padding: 3,
+    marginRight: 15,
+  },
+  wrong: {
+    backgroundColor: "red",
+    padding: 3,
+    marginRight: 15,
+  },
   container: {
-    backgroundColor:'#4286f4',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    padding: 10,
+    marginLeft: 10,
     flex: 1,
   },
   winner: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    marginLeft: 15,
-    padding: 10,
-    flexDirection: 'row',
   },
 });
 
