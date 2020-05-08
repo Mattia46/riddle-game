3e26051f3da0badaeec1b68241db92545d1e137f
Mattia46 <mattia.assogna@gmail.com>
2020-05-08 19:56:13 +0100

    removed subscription in main room and added refresh scroll down

--- components/RoomScreen/RoomScreen.js
+++ components/RoomScreen/RoomScreen.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Text, View, ScrollView, RefreshControl } from 'react-native';
 import { API, graphqlOperation } from 'aws-amplify';
 import { listUsers, listRiddles } from '../../src/graphql/queries';
 import { onUpdateRiddle, onCreateRiddle } from '../../src/graphql/subscriptions';
 import { Riddle } from './Riddle';
 import { styles } from './style';
 
-function NoRiddle({riddle}) {
+function NoRiddle({riddle, getTodayRiddle}) {
   if(riddle) return null;
+  const [refreshing, setRefreshing] = useState(false);
 
+  const onRefresh = () => {
+    getTodayRiddle();
+    setRefreshing(false);
+  };
   return (
     <View  style={styles.noRiddle}>
-      <Text style={{fontSize: 30, color: 'white', marginBottom: 10}}>Next riddle</Text>
-      <Text style={{fontSize: 17, color: 'white'}}>Tomorrow at 10:00</Text>
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+        <Text style={{fontSize: 30, color: 'white', marginBottom: 10}}>Next riddle</Text>
+        <Text style={{fontSize: 17, color: 'white'}}>Tomorrow at 10:00</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -25,34 +31,14 @@ export default function RoomScreen({user}) {
     .then(({data}) => setRiddle(data.listRiddles?.items[0]))
     .catch(err => alert('Error RoomScreen getTodayRiddle'));
 
-  const getOnCreateSub = () => API.graphql(graphqlOperation(onCreateRiddle)).subscribe({
-      next: ({value: { data: { onCreateRiddle }}}) => {
-        if(onCreateRiddle.date == today) return setRiddle(onCreateRiddle)
-      },
-      error: err => alert('Error RoomScreen onCreateRiddle sub')
-    });
-  const getOnUpdateSub = () => API.graphql(graphqlOperation(onUpdateRiddle)).subscribe({
-      next: ({value: { data: { onUpdateRiddle }}}) => {
-        if(onUpdateRiddle.date == today) return setRiddle(onUpdateRiddle)
-      },
-      error: err => alert('Error RoomScreen onUpdateRiddle sub')
-    });
-
   useEffect(() => {
-    const onCreateSub = getOnCreateSub();
-    const onUpdateSub = getOnUpdateSub();
     getTodayRiddle();
-
-    return () => {
-      onCreateSub.unsubscribe();
-      onUpdateSub.unsubscribe();
-    }
   }, []);
 
   return (
-    <React.Fragment>
+    <>
       <Riddle riddle={riddle} user={user} />
-      <NoRiddle riddle={riddle}/>
-    </React.Fragment>
+      <NoRiddle riddle={riddle} getTodayRiddle={getTodayRiddle}/>
+    </>
   );
 }
